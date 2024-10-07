bafb6f3455e4998e4140f12ea13602dbdd63269a
Mridul24agaga <mridulthareja8@gmail.com>
2024-10-07 23:15:37 +0530

    Asgg

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -43,7 +43,7 @@ export default function PostEditor() {
         italic: false,
       }),
       Placeholder.configure({
-        placeholder: "What's crack-a-lackin'?",
+        placeholder: "Please share your memories with us!",
       }),
     ],
   });
