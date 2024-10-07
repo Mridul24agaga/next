f84de37f3b3278d26b5b99538fee271364d33eb4
Mridul24agaga <mridulthareja8@gmail.com>
2024-10-07 17:42:04 +0530

    Hhash

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
         <Link href="/">
           <Image
-            src="https://media.discordapp.net/attachments/1172973820156592330/1276599067895664700/1_1.png?ex=66ca1d07&is=66c8cb87&hm=82e7369ef2470760bf2cc8453ecf36085cf75f149da543446c209cc74e563f33&=&format=webp&quality=lossless" // Replace with your logo URL
+            src="https://media.discordapp.net/attachments/1193183717548638301/1292820769419825222/Black_Gold_White_Luxury_Royal_Crown_Logo_1_2.png?ex=670520aa&is=6703cf2a&hm=835f06d7996d18321a60577412281862a4ad49d71c7c197ab0fe0832046a7daf&=&format=webp&quality=lossless" // Replace with your logo URL
             alt="Memories Lived Logo"
             width={150} // Adjust the width
             height={50} // Adjust the height
